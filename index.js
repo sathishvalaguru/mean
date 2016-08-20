e125f4657afb4dcb178bf104fcb571009cf1fd73
Sathish Valaguru <sathish.rage@gmail.com>
2016-08-20 15:25:25 +0530

    db settng

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -16,6 +17,18 @@ app.get('/', function(request, response) {
 
 app.get('/cool', function(request, response) {
   response.send(cool());
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
